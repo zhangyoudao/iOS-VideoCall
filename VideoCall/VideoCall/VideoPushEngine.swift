fa82f82016358b6bbad8ce33a46f25587bbf1d7b
zhangyoudao <a115@115deiMac-3.local>
2020-06-17 16:16:00 +0800

    导入金山云SDk

---
+++ VideoCall/VideoCall/VideoPushEngine.swift
@@ -0,0 +1,22 @@
+//
+//  VideoPushEngine.swift
+//  VideoCall
+//
+//  Created by 115 on 2020/6/17.
+//  Copyright © 2020 yun. All rights reserved.
+//
+
+import Foundation
+
+class VideoPushEngine: NSObject {
+    
+    
+    override init() {
+        super.init()
+        
+    }
+    
+    
+    
+    
+}
--- VideoCall/VideoCall/ViewController.swift
+++ VideoCall/VideoCall/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import libksygpulive
 
 class ViewController: UIViewController {
 
